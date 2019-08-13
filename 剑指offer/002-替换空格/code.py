086b19f75bee8e5148c55f4c00a06f06c879e639
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-14 02:08:28 +0800

    Update code.py

--- 剑指offer/002-替换空格/code.py
+++ 剑指offer/002-替换空格/code.py
@@ -1,23 +1,27 @@
+class Solution:
+    # s 源字符串
+    def replaceSpace(self, s):
+        # write code here
+        count_space = 0
+        for i in s:
+            if i == " ":
+                count_space += 1
+        len_s = len(s)
+        len_s += 2*count_space#替换成%20后多了2倍空格的字符
+        new_list = [" "]*len_s
+        j = 0
+        for i in range(len(s)):#遍历原字符串
+            if s[i] == " ":#遇到空格，在新列表中一次加入%20，下标前进3
+                new_list[j] = "%"
+                new_list[j+1] = "2"
+                new_list[j+2] = "0"
+                j += 3
+            else:
+                new_list[j] = s[i]
+                j += 1
+        return "".join(new_list)
 
-
-def replaceSpace(s):
-    # write code here
-    s_len = len(s)
-    space_count = 0
-    for i in s:
-        if i == ' ':
-            space_count += 1
-    s_len += 2 * space_count
-    new_array = [' '] * s_len
-    j = 0
-    for i in range(len(s)):
-        if s[i] == ' ':
-            new_array[j] = '%'
-            new_array[j+1] = '2'
-            new_array[j+2] = '0'
-            j += 3
-        else:
-            new_array[j] = s[i]
-            j += 1
-    return ''.join(new_array)
-print(replaceSpace('We Are Happy'))
+if __name__ ==  "__main__":
+    A = Solution()
+    SS = A.replaceSpace("dsa wd wqw wd")
+    print(SS)
