d45b92fc0cb35bc2b8beff909b4da114971a7e88
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-13 17:57:29 +0800

    Update solution.py

--- 剑指offer/038-二叉树的深度/solution.py
+++ 剑指offer/038-二叉树的深度/solution.py
@@ -5,18 +5,23 @@ class TreeNode:
         self.right = None
 
 
+#利用队列的特性，层次遍历每一层的节点，deep深度+1
 class Solution:
     def TreeDepth(self, pRoot):
         # write code here
-        if not pRoot: return 0
+        if not pRoot:
+            return 0
         queue = [pRoot]
         deep = 0
         while queue:
             n = len(queue)
             for _ in range(n):
+                # 将这一层的节点从队列中全部弹出
                 node = queue.pop(0)
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
+                # 将下一层的节点全部加入队列中
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
             deep += 1
         return deep
-                
