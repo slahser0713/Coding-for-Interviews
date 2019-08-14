b6e54d148bbb57ba0041c1122eee89e6e700a46d
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-15 01:27:10 +0800

    Update code.py

--- 剑指offer/005-用两个栈实现队列/code.py
+++ 剑指offer/005-用两个栈实现队列/code.py
@@ -2,15 +2,14 @@ class Solution:
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
-
+#用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
     def push(self, node):
         # write code here
         self.stack1.append(node)
-
-
     def pop(self):
         # return xx
+        #当stack2不为为空时才能从stack1往里面添加元素，否则先弹出stack2里面的元素
         if not self.stack2:
             while self.stack1:
-                self.stack2.append(self.stack1.pop(-1))
-        return self.stack2.pop(-1)
+                self.stack2.append(self.stack1.pop())                
+        return self.stack2.pop()
