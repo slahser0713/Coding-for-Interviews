0fb031de07768ed931ca74c534be4c6a3b064b3c
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-15 04:28:22 +0800

    Update code.py

--- 剑指offer/006-旋转数组的最小数字/code.py
+++ 剑指offer/006-旋转数组的最小数字/code.py
@@ -1,18 +1,21 @@
-def minNumberInRotateArray(rotateArray):
-    # write code here
-    if not rotateArray:
-        return 0
-    low = 0
-    high = len(rotateArray) - 1
-    while True:
-        mid = (low + high) // 2
-        if rotateArray[mid] >= rotateArray[low]:
-            low = mid
-        else:
-            high = mid
-        if rotateArray[mid] < rotateArray[mid-1]:
-            return rotateArray[mid]
+class Solution:
+    def minNumberInRotateArray(self, rotateArray):
+        # write code here
+        low = 0
+        high = len(rotateArray)-1
+        while low < high:#借助两个指针，做二分法
+            mid = (low+high) // 2
+            if rotateArray[mid] < rotateArray[mid - 1]:
+                return  rotateArray[mid]
+            if rotateArray[mid] < rotateArray[high]:
+                high = mid
+            elif rotateArray[mid] > rotateArray[high]:
+                low = mid+1
+            else:
+                high -= 1
+        return rotateArray[low]
 
-print(minNumberInRotateArray([3,4,5,1,2]))
-print(minNumberInRotateArray([1,1,1,0,1]))
-print(minNumberInRotateArray([]))
+a =Solution()
+print(a.minNumberInRotateArray([6501,6828,6963,7036,7422,7674,8146,8468,8704,8717,9170,9359,9719,9895,9896,
+                                9913,9962,154,293,334,492,1323,1479,1539,1727,1870,1943,2383,2392,2996,3282,
+                                3812,3903,4465,4605,4665,4772,4828,5142,5437,5448,5668,5706,5725,6300,6335]))
