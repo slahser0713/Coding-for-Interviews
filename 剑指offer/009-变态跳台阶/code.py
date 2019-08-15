adf19a4607c8085b8ebc62f35501b3ff5a1532ce
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-15 17:26:56 +0800

    Update code.py

--- 剑指offer/009-变态跳台阶/code.py
+++ 剑指offer/009-变态跳台阶/code.py
@@ -1,8 +1,11 @@
-def jumpFloorII(number):
-    # write code here
-    f1 = 1
-    if number == 1: return f1
-    for _ in range(number-1):
-        f1 = 2*f1
-    return f1
+class Solution:
+    def jumpFloorII(self, number):
+        res = [0,1,0]
+        if number < 2:
+            return res[number]
+        else:
+            for i in range(2,number+1):
+                res[2] = 2*res[1]
+                res[1] = res[2]
+            return res[2]
     
