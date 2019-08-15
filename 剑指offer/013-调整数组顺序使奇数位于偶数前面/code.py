fed0ef0b9daddbc5ad90959e926a13ed8acea7ff
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-15 23:27:56 +0800

    Update code.py

--- 剑指offer/013-调整数组顺序使奇数位于偶数前面/code.py
+++ 剑指offer/013-调整数组顺序使奇数位于偶数前面/code.py
@@ -1,21 +1,13 @@
-def reOrderArray(array):
-    # write code here
-    odd_cnt = 0
-    res = [0] * len(array)
-    # 统计个数
-    for n in array:  
-        if n % 2 != 0:
-            odd_cnt += 1
-    # 填坑
-    odd_i = 0
-    even_i = odd_cnt
-    for i in range(len(array)):
-        if array[i] % 2 != 0:  
-            res[odd_i] = array[i]
-            odd_i += 1
-        else:
-            res[even_i] = array[i]
-            even_i += 1
-    return res
-        
+class Solution:
+    def reOrderArray(self, array):
+        # write code here
+        odd =[]
+        even = []
+        for i in array:
+            if i % 2 ==1:
+                even.append(i)
+            else:
+                odd.append(i)
+        return even+odd
+
 
