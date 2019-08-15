d4bd788bdb8c22dd662469090a9cb692e8bd2809
wen <54058281+slahser0713@users.noreply.github.com>
2019-08-16 03:59:29 +0800

    Update code.py

--- 剑指offer/015-反转链表/code.py
+++ 剑指offer/015-反转链表/code.py
@@ -2,21 +2,18 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
-def ReverseList(pHead):
-    # write code here
-
-    if not pHead: return None
-    head = ListNode(0)
-    head.next = pHead
-    p = pHead
-    while(p.next):
-        tp = p.next
-        p.next = p.next.next
-        tp.next = head.next
-        head.next = tp
-    return head.next
-
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        # write code here
+        cur = pHead
+        pre = None
+        while cur:
+            nextnode = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nextnode
+        return pre
 
     # head->4->3->2->1->5
     #                p  tp
