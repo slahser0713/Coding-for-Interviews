7c57d9059ce4bf50975ea985bdb9551e74f36b2b
slahser0713 <wenyingkai@gmail.com>
2019-08-17 01:38:07 +0800

    剑指offer016

--- 剑指offer/016-合并有序链表/code.py
+++ 剑指offer/016-合并有序链表/code.py
@@ -2,23 +2,22 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
-def Merge(pHead1, pHead2):
-    # write code here
-    res = ListNode(0)
-    head = res
-    while pHead1 and pHead2:
-        if pHead1.val <= pHead2.val:
-            head.next = pHead1
-            head = head.next
-            pHead1 = pHead1.next
-        else:
-            head.next = pHead2
-            head = head.next
-            pHead2 = pHead2.next
-            
-    if pHead1:
-        head.next = pHead1
-    if pHead2:
-        head.next = pHead2
-    return res.next
+class Solution:
+    # 返回合并后列表
+    def Merge(self, pHead1, pHead2):
+        head = ListNode(0)
+        cur = head
+        while pHead2 and pHead1:
+            if pHead2.val >= pHead1.val:
+                cur.next = pHead1
+                cur = cur.next
+                pHead1 = pHead1.next
+            else:
+                cur.next = pHead2
+                cur = cur.next
+                pHead2 = pHead2.next
+        if pHead1:
+            cur.next = pHead1#cur,next接过来，后面的就全解上了
+        if pHead2:
+            cur.next = pHead2
+        return head.next
