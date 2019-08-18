03265f78eca9eb442891398eff6b7bd8d6b244ec
slahser0713 <wenyingkai@gmail.com>
2019-08-19 04:44:57 +0800

    剑指offer019

--- 剑指offer/020-包含min函数的栈/剑指020-包含min函数的栈.py
+++ 剑指offer/020-包含min函数的栈/剑指020-包含min函数的栈.py
@@ -2,26 +2,19 @@ class Solution:
     def __init__(self):
         self.stack = []
         self.min_stack = []
-
     def push(self, node):
-        # write code here
         self.stack.append(node)
         if not self.min_stack:
             self.min_stack.append(node)
-        else:
+        else:#记录当前最小值的最小栈，每加入一个数最小栈内也加入一个最小值保持一致
             if self.min_stack[-1] < node:
                 self.min_stack.append(self.min_stack[-1])
             else:
                 self.min_stack.append(node)
     def pop(self):
-        # write code here
-        self.stack.pop(-1)
-        self.min_stack.pop(-1)
-    
+        self.min_stack.pop()#当弹出一个数的时候，最小栈也弹出一个，回退到这个数进入之前的最小栈状态
+        return self.stack.pop()
     def top(self):
-        # write code here
-        return self.stack.pop(-1)
-
+        return self.stack[-1]
     def min(self):
-        # write code here
         return self.min_stack[-1]
