ea15ec297d3d03f65cc2feb5055a4fe3b8705e98
slahser0713 <wenyingkai@gmail.com>
2019-09-02 00:51:55 +0800

    剑指offer041

--- 剑指offer/041-和为S的连续正数序列/剑指041-和为S的连续正数序列.py
+++ 剑指offer/041-和为S的连续正数序列/剑指041-和为S的连续正数序列.py
@@ -1,18 +1,15 @@
 class Solution:
     def FindContinuousSequence(self, tsum):
-        # write code here
+        if tsum < 3:
+            return []
         res = []
         windows = []
-        sum = 0
-        for t in range(1, tsum):
-            windows.append(t)
-            sum += t
+        sum  = 0
+        for i in range(1,tsum // 2 + 2):
+            windows.append(i)
+            sum += i
             while sum > tsum:
                 sum -= windows.pop(0)
             if sum == tsum:
                 res.append(windows[:])
-
-        return res    
-
-print(Solution().FindContinuousSequence(100))
-        
+        return res
